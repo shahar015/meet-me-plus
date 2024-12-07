8bf42316d9ec8b8cb3cb7525721317ae60cb14c3
Shahar <shahar0151@gmail.com>
2024-12-08 00:37:43 +0200

    INIT

--- apps/meet-me-plus-client/tailwind.config.js
+++ apps/meet-me-plus-client/tailwind.config.js
@@ -13,5 +13,36 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  darkMode: ['selector', '[data-theme="dark-meet"]'],
+  daisyui: {
+    themes: [
+      {
+        'light-meet': {
+          primary: '#09bbe2',
+          secondary: '#54cfea',
+          accent: '#f4fcfe',
+          neutral: '#090909',
+          'base-100': '#fff6ec',
+          info: '#00d6ff',
+          success: '#00a900',
+          warning: '#f48500',
+          error: '#e11d48',
+        },
+      },
+      {
+        'dark-meet': {
+          primary: '#0f4c75',
+          secondary: '#3282b8',
+          accent: '#bbe1fa',
+          neutral: '#1b262c',
+          'base-100': '#fff6ec',
+          info: '#0f4c75',
+          success: '#166534',
+          warning: '#ca8a04',
+          error: '#9f1239',
+        },
+      },
+    ],
+  },
 };
